2b1d2275ec8e6dcf3e7a1a15a50ad2b067cbb5ce
Pablo Borges Martins <pablo.martins1998@gmail.com>
2020-03-30 01:19:24 -0300

    Validaçao de dados e testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes')
+const routes = require('./routes');
+const {errors} = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
 
 /**?
  * Métodos HTTP:
@@ -30,8 +34,3 @@ app.use(routes);
   * Driver: SELECT * FROM users
   * Query Builder: table('users').select('*').where()
   */
-
-
-
-
-app.listen(3333);
